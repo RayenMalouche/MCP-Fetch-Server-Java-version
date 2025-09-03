c95b50d35292b8ac7e85815bcc1320ab12891496
Rayen Malouche <rayenmalouche27@gmail.com>
2025-09-03 16:53:27 +0100

    Project Initialization

---
+++ src/main/java/com/mcp/RayenMalouche/java/server/Fetch/FetchApplication.java
@@ -0,0 +1,13 @@
+package com.mcp.RayenMalouche.java.server.Fetch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FetchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FetchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mcp/RayenMalouche/java/server/Fetch/FetchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcp.RayenMalouche.java.server.Fetch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FetchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
